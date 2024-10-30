0ab0b4a421f3a1e01ea5c947b82c50aae470b5f1
Aviationinfo720 <whatifyoucloseyour5@gmail.com>
2024-10-30 23:28:15 +1000

    updated python_weather usage script

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ import spotipy
 from discord.ui import View, Button
 from spotipy.oauth2 import SpotifyOAuth
 
-with open("PATH/TO/KEYS", "r") as f:
+with open("PATH/TO/FILE", "r") as f:
     keys = json.load(f)
 
 tracemalloc.start()
@@ -613,12 +613,12 @@ def generate_unix_time_code():
 
 @client.tree.command()
 async def get_weather(interaction: discord.Interaction, location: str) -> None:
+    """Gets weather for a city (suburb if whole city data isnt avalible)"""
     async with python_weather.Client(unit=python_weather.METRIC) as weather_client:
         # Get the weather for the specified location
         weather = await weather_client.get(location)
         embed = Embed(title=f"Weather for {location}, {weather.country}.", color=discord.Colour.from_rgb(r=117, g=204, b=255))
-        embed.description = f"""Location: **{weather.location}**
-                            Temperature: **{weather.temperature}°C**
+        embed.description = f"""**Temperature: **{weather.temperature}°C**
                             Weather Condition: **{weather.description}**"""
         embed.set_footer(text=f"Requested by {interaction.user.name}", icon_url=interaction.user.avatar.url)
 
